10062b85ddc14d23c210884a3f985d5317030bd2
Andres Cabrera <acabrera@mdsdigital.com>
2016-04-22 16:52:39 -0500

    React initial load.

--- SailsReact/j07/assets/js/index.js
+++ SailsReact/j07/assets/js/index.js
@@ -1,4 +1,11 @@
-import app from "./app.js";
-import nav from "./components/nav/nav.js";
-import header from "./components/header/header.js";
+import app from "./app.js"
+import nav from "./components/nav/nav.js"
+import header from "./components/header/header.js"
+
+
+import React from 'react'
+import {render} from 'react-dom'
+
+
+// https://www.codementor.io/reactjs/tutorial/beginner-guide-setup-reactjs-environment-npm-babel-6-webpack
 
